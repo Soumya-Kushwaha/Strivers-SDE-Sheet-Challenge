83340b75643df9d1ca04b5b9e24d9712e0dc3c02
Soumya Kushwaha <kushsoumya30@gmail.com>
2023-07-06 17:06:04 +0530

    Create 63_Single_Element.cpp

---
+++ 63_Single_Element.cpp
@@ -0,0 +1,45 @@
+/*
+int singleNonDuplicate(vector<int>& arr)
+{
+	int ans = 0;
+	for (int i=0; i<arr.size(); i++)
+		ans ^= arr[i];
+	return ans;
+    
+}
+*/
+
+//  Binary Search
+
+int singleNonDuplicate(vector<int>& nums) {
+	int n = nums.size() - 1;
+	if (n == 0)
+		return nums[0];
+
+	if (nums[0] != nums[1])
+		return nums[0];
+
+	if (nums[n] != nums[n-1])
+		return nums[n];
+
+	int low = 1, high = n - 1;
+	while (low <= high){
+		int mid = low + (high - low)/2;
+
+		if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+			return nums[mid];
+			
+		if (mid % 2 == 1){
+			if (nums[mid] == nums[mid - 1])
+				low = mid + 1;
+				
+			else high = mid - 1;
+		}
+		else{
+			if (nums[mid] == nums[mid + 1])
+				low = mid + 1;
+			else high = mid - 1;
+		}
+	}
+	return -1;
+}
